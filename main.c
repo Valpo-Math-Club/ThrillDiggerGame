cfb802b110422cf26530fc0439d0faaae870dff0
Spencer Gannon <spencer.gannon@valpo.edu>
2023-09-11 19:35:15 -0500

    Removed isBad in favor of arrayHasElement

--- main.c
+++ main.c
@@ -22,17 +22,16 @@ const int  THRESHOLDS[NUM_RUPEE_TYPES] = {0, 2, 4, 6, 8};
 const int  VALUES[NUM_RUPEE_TYPES] = {1, 5, 20, 100, 300};
 const char BOARD_CHARS[NUM_RUPEE_TYPES] = {'G', 'B', 'R', 'S', 'A'};
 // functions
-int  numBadNeighbors(int rows, int cols, int testIndex);
-int  arrayHasElement(int* arr, int length, int value);
+int  numBadNeighbors(int* bomb_indices, int rows, int cols, int testIndex);
 void print_arr(int* arr, int arr_size);
 void print_board(char* arr, int rows, int cols);
-bool isBad(int index);
-
-int bomb_indices[NUM_BOMBS];
+bool arrayHasElement(int* arr, int length, int value);
 
 int main(void) {
 	int playerCredit = 0;
+
 	char buffer[100];
+	int bomb_indices[NUM_BOMBS];
 	char* gameboard = calloc(BOARD_SIZE, sizeof(char));
 
 	srand(time(NULL));
@@ -53,36 +52,32 @@ int main(void) {
 		if(DEBUG_STATEMENTS) sleep(1);
 	}
 
-	// each turn goes like this
-	
 	print_board(gameboard, ROWS, COLS);
 	fflush(stdout);
 
 	while(true) {
-		int userRow;
-		int userCol;
+		int userRow, userCol;
 		char userColChar;
-
 		bool outside_row_range, outside_col_range, non_empty_space;
+
 		do {
 			userRow = userCol = -1;
 			fgets(buffer, sizeof(buffer), stdin);
 			sscanf(buffer, "%c %d", &userColChar, &userRow);
 
 			fflush(stdin);
-
-			// quicker atoi
-			userCol = userColChar - 'a';
+			
+			userCol = userColChar - 'a'; // quicker atoi
 
 			outside_row_range = (userRow < 0 || userRow >= ROWS);
 			outside_col_range = (userCol < 0 || userCol >= COLS);
-			non_empty_space = gameboard[userRow*COLS + userCol] != ' ';
+			non_empty_space   = gameboard[userRow*COLS + userCol] != ' ';
 		} while(outside_row_range || outside_col_range || non_empty_space);
 
 		int userIndex = (userRow * COLS) + userCol;
-		int badNeighbors = numBadNeighbors(ROWS, COLS, userIndex);
+		int badNeighbors = numBadNeighbors(bomb_indices, ROWS, COLS, userIndex);
 
-		if(isBad(userIndex)) {
+		if(arrayHasElement(bomb_indices, userIndex)) {
 			printf("GAME OVER ):<\n");
 			break;
 		}
@@ -103,81 +98,54 @@ int main(void) {
 	return 0;
 }
 
-// check if the element "index" appears in "bomb_indices"
-bool isBad(int index) {
-	for(int i = 0; i < NUM_BOMBS; i++)
-		if(bomb_indices[i] == index) return true;
-
+bool arrayHasElement(int* arr, int length, int value){
+	for(int i = 0; i < length; i++)
+		if(arr[i] == value) return true;
+	
 	return false;
 }
 
-int numBadNeighbors(int rows, int cols, int testIndex) {
+int numBadNeighbors(int* bomb_indices, int rows, int cols, int testIndex) {
 	int result = 0;
 
-	bool hasLeftNeighbor = (testIndex % cols != 0);
-	bool hasRightNeighbor = ((testIndex+1) % cols != 0);
-
-	bool hasUpperNeighbor = (testIndex / cols != 0);
-	bool hasLowerNeighbor = (testIndex / cols != rows - 1);
-
-
-	// check upper left neighbor
-	if(hasUpperNeighbor && hasLeftNeighbor) {
-		if(isBad(testIndex - 1 - cols)) {
-			result++;
+	bool hasLeftNeighbors  = ((testIndex % cols) != 0);
+	bool hasRightNeighbors = (((testIndex + 1) % cols ) != 0);
+	bool hasUpperNeighbors = ((testIndex / cols) != 0);
+	bool hasLowerNeighbors = ((testIndex / cols) != (rows - 1));
+	// there's definitely a better way to do this, but this is more concise
+	// to-do: refactor later
+	if(hasUpperNeighbors) {
+		if(hasLeftNeighbors){
+			if(arrayHasElement(bomb_indices, testIndex - 1 - cols))
+				result++;
 		}
-	}
-
-	// check upper neighbor
-	if(hasUpperNeighbor) {
-		if(isBad(testIndex - cols)) {
-			result++;
+		if(hasRightNeighbors){
+			if(arrayHasElement(bomb_indices, testIndex + 1 - cols))
+				result++;
 		}
-	}
-
-	// check upper right neighbor
-	if(hasUpperNeighbor && hasRightNeighbor) {
-		if(isBad(testIndex + 1 - cols)) {
+		if(arrayHasElement(bomb_indices, testIndex - cols))
 			result++;
-		}
 	}
-
-	// check left neighbor
-	if(hasLeftNeighbor) {
-		if(isBad(testIndex - 1)) {
+	if(hasLowerNeighbors){
+		if(hasLeftNeighbors){
+			if(arrayHasElement(bomb_indices, testIndex + cols - 1))
+				result++;
+		}
+		if(hasRightNeighbors){
+			if(arrayHasElement(bomb_indices, testIndex + cols + 1))
+				result++;
+		}
+		if(arrayHasElement(bomb_indices, testIndex + cols))
 			result++;
-		}
 	}
-
-	// check right neighbor
-	if(hasRightNeighbor) {
-		if(isBad(testIndex + 1)) {
+	if(hasLeftNeighbors){
+		if(arrayHasElement(bomb_indices, testIndex - 1))
 			result++;
-		}
 	}
-
-	// check lower left neighbor
-	if(hasLowerNeighbor && hasLeftNeighbor) {
-		if(isBad(testIndex + cols - 1)) {
+	if(hasRightNeighbors){
+		if(arrayHasElement(bomb_indices, testIndex + 1))
 			result++;
-		}
 	}
-
-	// check lower neighbor
-	if(hasLowerNeighbor) {
-		if(isBad(testIndex + cols)) {
-			result++;
-		}
-	}
-
-	// check lower right neighbor
-	if(hasLowerNeighbor && hasRightNeighbor) {
-		if(isBad(testIndex + cols + 1)) {
-			result++;
-		}
-	}
-
-
 
 	return result;
 }
